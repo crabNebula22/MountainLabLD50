f9b66e067930f4ac2b36ecaf492eec92a40e8e31
crabNebula22 <102708418+crabNebula22@users.noreply.github.com>
2022-04-02 12:42:20 -0600

    Update text.lua

--- src/constants/text.lua
+++ src/constants/text.lua
@@ -1,5 +1,5 @@
 
 gText = {
-    ['introText'] = 'You are the Pontifex in Rome and Vesuvius is rumbling. Help the villagers avoid certain destruction by divining what should be sacrificed! Drag objects in the volcano to see if it can be pacified',
+    ['introText'] = 'As Pontifex Maximus you have been summoned to Pompeii to quell the rumblings of Vesuvius! Watch for the auspices in the skies and earth to divine the required sacrifice for the gods. Drag the victim to the top of the Volcano and see if your reading is correct.',
     ['startScreen'] = 'press enter to begin'
 }
